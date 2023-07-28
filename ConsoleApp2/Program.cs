9cd10b691404b9488ab798b434aa46b9900dfa30
kamalSaiDd <44686078+kamalSaiDd@users.noreply.github.com>
2023-07-28 06:52:04 +0530

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] oddArray = new int[] { 1, 3, 5 };
+        int i = 0;
+        while (i < oddArray.Length)
+        {
+            Console.WriteLine(oddArray[i]);
+            i++;
+        }
+        Console.ReadKey();
+    }
+}
